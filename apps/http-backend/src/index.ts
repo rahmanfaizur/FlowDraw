b7a1ab006f5bbc276250f095d8acbc35a49701c9
rahmanfaizur <rahman.faizur.fr@gmail.com>
2025-02-07 19:53:38 +0530

    Added prisma

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,26 +1,41 @@
 import express from "express";
 import z from "zod";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/v1/signup', async (req, res) => {
-    const userSchema = z.object({
-        username: z.string().min(1, "Username is Required!"),
-        password: z.string().min(6, "Password must be at least 6 character long!")
-    });
+
+//zod schemas
+const userSchema = z.object({
+    username: z.string().min(1, "Username is Required!"),
+    password: z.string().min(6, "Password must be at least 6 character long!")
+});
+
+const roomQuerySchema = z.object({
+    id: z.string().uuid()
+});
+
+//routes
+
+app.post('/api/v1/signup', async (req, res) => {
     try {
         const { username, password } = userSchema.parse(req.body);
         const hashedPassword: string = await bcrypt.hash(password, 10);
         //here comes the duplicate user logic check and hence the db logic!
         //if no then insert the user and the password to the db!
-        res.status(400).json({
+        res.status(201).json({
             message: "User created successfully!"
         })
     }
     catch(err) {
+        if (err instanceof z.ZodError) {
+            res.status(400).json({
+                message: "Validation Error", errors: err.errors
+            });
+        }
         res.status(500).json({
             message: "You did something wonky!"
         });
@@ -28,16 +43,30 @@ app.get('/api/v1/signup', async (req, res) => {
     }
 })
 
-app.get('/api/v1/signin', (req, res) => {
-    res.json({
-        message: "Http backend hit!"
-    })
+app.post('/api/v1/signin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        //find user with the same username in the database
+        //if not give an error
+
+        //password check from d (the hashed password)
+
+        //jwt logic
+        // const token = jwt.sign(
+        //     //id
+        //     // JWT_PASS
+        // )
+    }
+    catch (err) {
+        res.status(400).json({
+            message: "Error caught!"
+        })
+    }
 })
 
-app.get('/api/v1/room', (req, res) => {
-    res.json({
-        message: "Http backend hit!"
-    })
+app.post('/api/v1/room', (req, res) => {
+    const validation = roomQuerySchema.safeParse(req.body);
 })
 
 
